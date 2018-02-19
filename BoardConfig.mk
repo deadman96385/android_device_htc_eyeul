f6bb56e5c297013e9cb39a01ad51044227896196
Ethan Chen <intervigil@gmail.com>
2018-02-19 12:39:55 -0800

    hima: Disambiguate camera parameter lib name
    Change-Id: I115129903e09563d4782eb1ee8aad222eafc54ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,8 +45,7 @@ TARGET_KERNEL_CONFIG := eyeul_defconfig
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/eyeul/bluetooth
 
 # Camera
-COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND='{"htc.camera.sensor.", AID_CAMERA, 0}, {"camera.4k2k.", AID_MEDIA, 0},'
+TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := libcamera_parameters_ext_eyeul
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Includes
