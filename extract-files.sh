c6e740f320512ea1687b74660d7a61ee47152468
deadman96385 <seanhoyt963@gmail.com>
2020-05-03 14:32:14 -0500

    eyeul: Update file extractors to match new template

--- extract-files.sh
+++ extract-files.sh
@@ -1,43 +1,64 @@
 #!/bin/bash
+#
+# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
 export DEVICE=eyeul
 export VENDOR=htc
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
     exit 1
-  fi
+fi
+. "$HELPER"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --path )           shift
+                                SRC=$1
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                CLEAN_VENDOR=false
+                                ;;
+        -n | --no-cleanup )     CLEAN_VENDOR=false
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
 fi
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
-if [ -f ../$DEVICE/proprietary-files.txt ]; then
-  for FILE in `egrep -v '(^#|^$)' ../$DEVICE/proprietary-files.txt`; do
-    FILE=`echo ${FILE[0]} | sed -e "s/^-//g"`
-    echo "Extracting /system/$FILE ..."
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-      mkdir -p $BASE/$DIR
-    fi
-    if [ "$SRC" = "adb" ]; then
-      adb pull /system/$FILE $BASE/$FILE
-    else
-      cp $SRC/system/$FILE $BASE/$FILE
-    fi
-  done
-fi
+extract "$MY_DIR"/device-proprietary-files.txt "$SRC" "$SECTION"
 
-../$DEVICE/setup-makefiles.sh
+"$MY_DIR"/setup-makefiles.sh
